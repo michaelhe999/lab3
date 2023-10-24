11ff4847c18f832a3b159099e8543697a44394b0
Michael He <michaelhe2016@gmail.com>
2023-10-24 12:52:19 -0700

    Fixed ArrayExamples, wrote some ArrayTests

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,20 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test 
+  public void testReversedWorking(){
+      int[] input = {0,1,2,3,4};
+      int[] input2 = {0,1,2,3,4};
+      int[] output = {4,3,2,1,0};
+      ArrayExamples.reverseInPlace(input);
+      assertArrayEquals(output, input);
+      assertArrayEquals(output, ArrayExamples.reversed(input2));
+  }
+
+  @Test
+  public void testAverageWithoutLowest(){
+    double[] input = {1,1,2,3,4};
+    assertEquals(2.5, ArrayExamples.averageWithoutLowest(input));
+  }
 }
